8f6de8cc06d76c2b6d4fb73ef11922bb89e81ed5
Nicolomecca <nico97.mecca@gmail.com>
2024-11-06 14:45:36 +0100

    creazione progetto

---
+++ W6_d3/src/main/java/Nicolo_Mecca/W6_d3/W6D3Application.java
@@ -0,0 +1,13 @@
+package Nicolo_Mecca.W6_d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W6D3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W6D3Application.class, args);
+	}
+
+}
---
+++ W6_d3/src/test/java/Nicolo_Mecca/W6_d3/W6D3ApplicationTests.java
@@ -0,0 +1,13 @@
+package Nicolo_Mecca.W6_d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W6D3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
